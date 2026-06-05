6c1f9e404d1e3ec536f6d3fa337eb0d5419f9bf3
ljbtorres-debug <ljb.torres@yavirac.edu.ec>
2026-06-04 19:44:32 -0500

    prueba 3

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ HTML = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola Mundo Flask</title>
+    <title>Hola Mundo byron</title>
 
     <style>
         *{
