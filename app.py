0b7c5874c05998148b34a80fd0a22b52ff7b44a6
ljbtorres-debug <ljb.torres@yavirac.edu.ec>
2026-06-04 21:58:19 -0500

    intento4

--- app.py
+++ app.py
@@ -1,92 +1,40 @@
-from flask import Flask, render_template_string
+from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
-HTML = """
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola Mundo byron</title>
-
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        body{
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: linear-gradient(135deg, #0f172a, #1e293b);
-            color: white;
-        }
-
-        .card{
-            background: rgba(255,255,255,0.08);
-            backdrop-filter: blur(10px);
-            padding: 40px;
-            border-radius: 20px;
-            text-align: center;
-            width: 400px;
-            box-shadow: 0 10px 30px rgba(0,0,0,0.3);
-            border: 1px solid rgba(255,255,255,0.1);
-        }
-
-        h1{
-            font-size: 3rem;
-            margin-bottom: 15px;
-        }
-
-        p{
-            color: #cbd5e1;
-            margin-bottom: 25px;
-            line-height: 1.5;
-        }
-
-        .button{
-            display: inline-block;
-            padding: 12px 24px;
-            border-radius: 10px;
-            background: #38bdf8;
-            color: white;
-            text-decoration: none;
-            font-weight: bold;
-            transition: 0.3s;
-        }
-
-        .button:hover{
-            background: #0ea5e9;
-            transform: translateY(-2px);
-        }
-    </style>
-</head>
-<body>
-
-    <div class="card">
-        <h1>Hola byron ponle 10/h1>
-
-        <p>
-            Tu aplicación Flask está funcionando correctamente.
-        </p>
-
-        <a href="/" class="button">
-            Recargar
-        </a>
-    </div>
-
-</body>
-</html>
-"""
+VERSION = "3.0.0"
 
 @app.route("/")
-def home():
-    return render_template_string(HTML)
+def inicio():
+
+    try:
+
+        conexion = psycopg2.connect(
+            host="db",
+            database="empresa",
+            user="admin",
+            password="admin123"
+        )
+
+        cursor = conexion.cursor()
+
+        cursor.execute("SELECT version();")
+
+        version = cursor.fetchone()
+
+        cursor.close()
+        conexion.close()
+
+        return f"""
+        <h1>Aplicación Flask</h1>
+        <h2>Versión {VERSION}</h2>
+        <p>Conexión exitosa a PostgreSQL</p>
+        <p>{version}</p>
+        """
+
+    except Exception as e:
+        return str(e)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001)
